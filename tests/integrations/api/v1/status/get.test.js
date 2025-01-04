9ef7849e3f28acaaef5c1ccc706a0dff019caf02
Matheus Donadoni <matheusgoncalvesdonadoni@gmail.com>
2025-01-04 16:57:00 -0300

    test: rename all description to use new pattern

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -4,25 +4,29 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("Get to /api/v1/status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(parseUpdatedAt).toEqual(responseBody.updated_at);
+      const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(parseUpdatedAt).toEqual(responseBody.updated_at);
 
-  const databaseVersion = responseBody.dependencies.database.version;
-  expect(databaseVersion).toEqual("16.0");
+      const databaseVersion = responseBody.dependencies.database.version;
+      expect(databaseVersion).toEqual("16.0");
 
-  const databaseMaxConnection =
-    responseBody.dependencies.database.max_connections;
-  expect(databaseMaxConnection).toEqual(100);
+      const databaseMaxConnection =
+        responseBody.dependencies.database.max_connections;
+      expect(databaseMaxConnection).toEqual(100);
 
-  const databaseOpenedConnections =
-    responseBody.dependencies.database.opened_connections;
+      const databaseOpenedConnections =
+        responseBody.dependencies.database.opened_connections;
 
-  expect(databaseOpenedConnections).toEqual(1);
+      expect(databaseOpenedConnections).toEqual(1);
+    });
+  });
 });
